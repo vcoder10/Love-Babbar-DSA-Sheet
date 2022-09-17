27efc125db180599fcaf7639d7900e22b894ecae
vcoder10 <65166636+vcoder10@users.noreply.github.com>
2022-09-18 04:02:02 +0530

    best till now

---
+++ Array/8.Minimize the height.cpp
@@ -0,0 +1,73 @@
+/*
+
+Minimize the Heights II
+MediumAccuracy: 28.62%Submissions: 100k+Points: 4
+Lamp
+This problem is part of GFG SDE Sheet. Click here to view more.  
+
+Given an array arr[] denoting heights of N towers and a positive integer K.
+
+For each tower, you must perform exactly one of the following operations exactly once.
+
+Increase the height of the tower by K
+Decrease the height of the tower by K
+Find out the minimum possible difference between the height of the shortest and tallest towers after you have modified each tower.
+
+You can find a slight modification of the problem here.
+Note: It is compulsory to increase or decrease the height by K for each tower. After the operation, the resultant array should not contain any negative integers.
+
+
+Example 1:
+
+Input:
+K = 2, N = 4
+Arr[] = {1, 5, 8, 10}
+Output:
+5
+Explanation:
+The array can be modified as 
+{3, 3, 6, 8}. The difference between 
+the largest and the smallest is 8-3 = 5.
+Example 2:
+
+Input:
+K = 3, N = 5
+Arr[] = {3, 9, 12, 16, 20}
+Output:
+11
+Explanation:
+The array can be modified as
+{6, 12, 9, 13, 17}. The difference between 
+the largest and the smallest is 17-6 = 11. 
+
+Your Task:
+You don't need to read input or print anything. Your task is to complete the function getMinDiff() which takes the arr[], n, and k as input parameters and returns an integer denoting the minimum difference.
+
+
+Expected Time Complexity: O(N*logN)
+Expected Auxiliary Space: O(N)
+
+Constraints
+1 ≤ K ≤ 104
+1 ≤ N ≤ 105
+1 ≤ Arr[i] ≤ 105
+
+*/
+// step 1 : we have to sort 
+//
+ sort(arr, arr+n);
+       int ans = arr[n - 1] - arr[0];
+       int smallest = arr[0] + k;
+       int largest = arr[n - 1] - k;
+       int maxi, mini;
+//
+       // finding new values
+       for (int i = 0; i < n - 1; i++) {
+           maxi = max(largest, arr[i] + k);
+           mini = min(smallest, arr[i + 1] - k);
+           if (mini < 0)
+               continue;
+//
+           ans = min(ans, maxi - mini);
+       }
+       return ans;
